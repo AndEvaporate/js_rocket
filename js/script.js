e152ab7fa9bccfe3c10d392525664432e7349c10
AndEvaporate <63962047+AndEvaporate@users.noreply.github.com>
2020-05-22 09:13:07 -0400

    added more JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 
 
+
+let	doCoolStuff = function () {
+	let currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	}
+
+	else {
+		document.getElementById('cool').className = 'cool';
+	}
+
+}
+
+
+
+
 	var car = {
 			make: 'VW',
 			type: 'Polo',
